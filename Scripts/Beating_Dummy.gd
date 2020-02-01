d51ce479f268f858d78e31356e0a890e057dd7ee
PromZona <promzona17112000@mail.ru>
2020-02-01 15:32:41 +0300

    init

--- Scripts/Beating_Dummy.gd
+++ Scripts/Beating_Dummy.gd
@@ -1,19 +1,12 @@
-extends KinematicBody2D
+extends Area2D
 
-func _start():
-	get_node("DamageArea/CollisionShape2D3").set_disabled(false)
 
-func _on_CollisionShape2D_area_entered(area):
-	if (get_node("Sprite/AnimationPlayer").is_playing() == true and 
-		get_node("DamageArea/CollisionShape2D3").is_disabled() == false):
-			
-		if (area.is_in_group("player")):
-			area.take_damage()
-	
-	if area.is_in_group("player") and get_node("Sprite/AnimationPlayer").is_playing() == false:
-		get_node("Sprite/AnimationPlayer").play("Boom")
 
-func _on_AnimationPlayer_animation_finished(anim_name):
-	get_node("Sprite/AnimationPlayer").clear_caches()
-	get_node("DamageArea/CollisionShape2D3").set_disabled(false)
-	
+func _ready():
+	pass # Replace with function body.
+
+
+func take_damage():
+	get_parent().takeDamage()
+	get_parent().queue_free()
+
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,8 +6,6 @@ var anim_node: AnimationPlayer
 # to detect collisions with enemies
 var enemy_collision_detector: CollisionObject2D
 
-var invincible_timer: Timer
-
 # b r u h
 # pretty sure this can be made as one block
 # but ehm..
@@ -19,6 +17,9 @@ var enemy_melee_left: Area2D
 var direction = Vector2.ZERO
 var hp = 100
 
+var canLit : bool = false
+var lanternInArea
+
 func _ready():
 	sprite_node = get_node("Sprite") # Used in flipping the character when moving
 	left_attack = get_node("LeftHit/Sprite/AnimationPlayer")
@@ -28,15 +29,14 @@ func _ready():
 	enemy_melee_left = get_node("LeftHit")
 	enemy_melee_right = get_node("RightHit")
 	
-	invincible_timer = get_node("Timer")
-	invincible_timer.set("one_shot", true)
-	
-	get_node("HUD_Layer/HUD/VBoxContainer/Bars/Bar/Gauge").set_value(hp)
+	var allScene = get_parent().get_children()
+	for i in allScene:
+		i.connect("CanLit", self, "enterLanternArea")
+		i.connect("CannotLit", self, "exitLanternArea")
+		
+
 
 func _process(delta):
-	if hp <= 0:
-		pass # End of the night round
-		
 	if Input.is_action_just_pressed("attack_melee"):
 		print("attack ")
 		if (sprite_node.flip_h == true): 
@@ -45,15 +45,11 @@ func _process(delta):
 		else:
 			print("left")
 			left_attack.play("Strike1")
+	if(Input.is_action_just_pressed("Use")):
+		lanternInArea.lit()
 
-func take_damage():
-	if invincible_timer.get("time_left") == 0:
-		print("hp down")
-		hp -= 25
-		invincible_timer.start(2)
-	else:
-		print("invincible")
-	get_node("HUD_Layer/HUD/VBoxContainer/Bars/Bar/Gauge").set_value(hp)	
+func detect_enemy_overlap():
+	pass
 
 func _physics_process(delta):
 	var direction = get_direction()
@@ -66,10 +62,9 @@ func _physics_process(delta):
 	velocity = calculate_move_velocity(velocity, speed, direction)
 
 	if velocity.x != 0:
-		anim_node.play("Walking")
+		anim_node.set_autoplay("Walking")
 	else:
-		anim_node.stop(1)
-		anim_node.clear_caches()
+		anim_node.stop(0)
 
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 	
@@ -94,18 +89,24 @@ func calculate_move_velocity(
 
 # detects if enemy is in the range of left hit area
 func _on_LeftHit_area_entered(area):
-	if (not area.is_in_group("Neutral")):
-		if area.is_in_group("hit"):
-			print("hit")
-			area.take_damage()
+	if area.is_in_group("hit"):
+		print("hit")
+		area.take_damage()
 	else:
 		print("uh..")
 
 # detects if enemy is in the range of right hit area
 func _on_RightHit_area_entered(area):
-	if (not area.is_in_group("Neutral")): 
-		if area.is_in_group("hit"):
-			print("hit")
-			area.take_damage()
+	if area.is_in_group("hit"):
+		print("hit")
+		area.take_damage()
 	else:
 		print("uhh..")
+
+func enterLanternArea(lantern):
+	lanternInArea = lantern
+	canLit = true
+
+func exitLanternArea():
+	lanternInArea = null
+	canLit = false
